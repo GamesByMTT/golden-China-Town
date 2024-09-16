48cb96b11b722b9928c5a043003eca66c41c5732
arpitrana717 <arpitrana.underpinservices@gmail.com>
2024-09-16 10:12:11 +0530

    remove gameObject in Loader Scene

--- src/view/MainLoader.ts
+++ src/view/MainLoader.ts
@@ -4,17 +4,11 @@ import { Scene, GameObjects } from "phaser";
 import MainScene from "./MainScene";
 import { LoaderConfig, LoaderSoundConfig } from "../scripts/LoaderConfig";
 import { Globals } from "../scripts/Globals";
-import { SceneHandler } from "../scripts/SceneHandler";
 import SoundManager from "../scripts/SoundManager";
 import { Howl } from "howler";
 
 export default class MainLoader extends Scene {
     resources: any;
-    private progressBar: GameObjects.Sprite | null = null;
-    private progressBox: GameObjects.Sprite | null = null;
-    private logoImage: GameObjects.Sprite | null = null;
-    private maxProgress: number = 0.7; // Cap progress at 70%
-    private backgroundMusic: Phaser.Sound.BaseSound | null = null; // Add a variable for background music
     public soundManager: SoundManager; // Add a SoundManager instance
     isAssestLoaded: boolean = false
 
@@ -39,31 +33,17 @@ export default class MainLoader extends Scene {
             }
         });
 
-        setTimeout(() => {
-            this.load.on('complete', () => {
-                console.log("completecompletecompletecomplete");
-                
-                // Only complete progress after socket initialization
-                if (Globals.Socket?.socketLoaded) {
+        this.load.on('complete', () => {
+            console.log("completecompletecompletecomplete");
+            if (Globals.Socket?.socketLoaded) {
                     this.completeLoading();
-                }
-            });
-        }, 3000);
+            }
+        });
     }
 
 
     private completeLoading() {
         console.log("completeLoading", this.isAssestLoaded, Globals.Socket?.socketLoaded);
-        // if (this.progressBox) {
-        //     this.progressBox.destroy();
-        // }
-        // if (this.progressBar) {
-        //     this.progressBar.destroy();
-        // }
-        // if(this.logoImage){
-        //     this.logoImage.destroy();
-        // }
-        // this.updateProgressBar(1); // Set progress to 100%
         const loadedTextures = this.textures.list;
         Globals.resources = { ...loadedTextures }
         Object.entries(LoaderSoundConfig).forEach(([key]) => {
@@ -81,6 +61,8 @@ export default class MainLoader extends Scene {
         console.log("loadScene", this.isAssestLoaded, Globals.Socket?.socketLoaded);
         // this.completeLoading();
         if(this.isAssestLoaded && Globals.Socket?.socketLoaded){
+            console.log("echeck");
+            
             window.parent.postMessage("OnEnter", "*")
         }
             Globals.SceneHandler?.addScene('MainScene', MainScene, true)
